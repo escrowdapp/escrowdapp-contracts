d8f2bb57e23389786f312e85c8eea81c9458c26e
karaoglan <burakkaraoglan55@gmail.com>
2022-11-03 15:04:11 +0300

    add files

---
+++ Escrow.sol
@@ -0,0 +1,237 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import './SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+contract Escrow {
+    using SafeMath for uint256;
+    uint256 duration = 915151608; //29 years default
+    uint8 feePercent = 1; //default 1
+    uint256 deliverRejectDuration = 86400;
+
+    enum EscrowStatus {
+        Launched,
+        Ongoing,
+        Delivered,
+        RequestRevised,
+        Dispute,
+        Cancelled,
+        Complete
+    }
+
+    struct EscrowDetail {
+        EscrowStatus status;
+        bytes32 title;
+        bytes32 shortInfo;
+        address tokenAddress;
+        uint256 deadline;
+        address payable buyer;
+        address payable seller;
+        uint256 requestRevisedDeadline;
+        uint256 amount;
+        address escrowAddress;
+    }
+
+    EscrowDetail escrowDetail;
+
+    address payable public addressToPayFee;
+
+    uint256 rejectCount = 0;
+
+    address public tokenAddress;
+
+    mapping(address => bool) public areTrustedHandlers;
+
+    constructor(
+        address payable _addressToPayFee,
+        address _tokenAddress,
+        uint256 _duration,
+        uint256 amount,
+        bytes32 title,
+        bytes32 shortInfo,
+        address payable buyer,
+        address payable seller,
+        uint8 _feePercent,
+        address[] memory _handlers
+    ) {
+        if (_duration > 0) {
+            duration = _duration;
+        }
+        addressToPayFee = _addressToPayFee;
+        tokenAddress = _tokenAddress;
+        feePercent = _feePercent;
+        areTrustedHandlers[msg.sender] = true;
+        addTrustedHandlers(_handlers);
+        escrowDetail = EscrowDetail(
+            EscrowStatus.Launched,
+            title,
+            shortInfo,
+            _tokenAddress,
+            duration.add(block.timestamp),
+            buyer,
+            seller,
+            0,
+            amount,
+            address(this)
+        ); // solhint-disable-line not-rely-on-time
+    }
+
+    fallback() external payable {
+        escrowDetail.amount = msg.value;
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        if (tokenAddress == address(0)) {
+            return address(this).balance;
+        }
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function addTrustedHandlers(address[] memory _handlers) public trusted {
+        for (uint256 i = 0; i < _handlers.length; i++) {
+            areTrustedHandlers[_handlers[i]] = true;
+        }
+    }
+
+    function sendAndStatusUpdate(address payable toFund, EscrowStatus status)
+        private
+    {
+        uint256 fee = escrowDetail.amount.mul(feePercent).div(100); // %1
+        if (tokenAddress == address(0)) {
+            addressToPayFee.transfer(fee); // %1
+            toFund.transfer(escrowDetail.amount - fee);
+        } else {
+            IERC20 token = IERC20(tokenAddress);
+            token.transfer(addressToPayFee, fee); // %1
+            token.transfer(toFund, escrowDetail.amount - fee);
+        }
+        escrowDetail.status = status;
+    }
+
+    function sellerLaunchedApprove() public payable onlySeller {
+        require(getBalance() > 0, '___NO_FUNDS___');
+        require(
+            escrowDetail.status == EscrowStatus.Launched,
+            '___NOT_IN_LAUNCHED_STATUS___'
+        );
+        escrowDetail.status = EscrowStatus.Ongoing;
+    }
+
+    function sellerDeliver() external onlySeller {
+        require(
+            escrowDetail.status == EscrowStatus.Ongoing,
+            '___NOT_IN_ONGOING_STATUS___'
+        );
+        escrowDetail.status = EscrowStatus.Delivered;
+    }
+
+    function buyerConfirmDelivery() external payable onlyBuyer {
+        require(
+            escrowDetail.status == EscrowStatus.Delivered,
+            '___NOT_IN_DELIVERED_STATUS___'
+        );
+        sendAndStatusUpdate(escrowDetail.seller, EscrowStatus.Complete);
+    }
+
+    function buyerDeliverReject(uint256 _deliverRejectDuration)
+        external
+        onlyBuyer
+    {
+        require(
+            escrowDetail.status == EscrowStatus.Delivered,
+            '___NOT_IN_DELIVERED_STATUS___'
+        );
+        require(_deliverRejectDuration >= 86400, '___REJECT_MIN_DAY___'); //1 day min
+        rejectCount++;
+        EscrowStatus state = EscrowStatus.RequestRevised;
+        if (rejectCount > 1) {
+            state = EscrowStatus.Dispute;
+            escrowDetail.status = state;
+        } else {
+            escrowDetail.status = state;
+            deliverRejectDuration = _deliverRejectDuration;
+            escrowDetail.requestRevisedDeadline = deliverRejectDuration.add(
+                block.timestamp
+            );
+        }
+    }
+
+    function sellerRejectDeliverReject() external onlySeller {
+        require(
+            escrowDetail.status == EscrowStatus.RequestRevised,
+            '___NOT_IN_REJECT_DELIVERY_STATUS___'
+        );
+        escrowDetail.status = EscrowStatus.Dispute;
+    }
+
+    function sellerApproveDeliverReject() external onlySeller {
+        require(
+            escrowDetail.status == EscrowStatus.RequestRevised,
+            '___NOT_IN_REJECT_DELIVERY_STATUS___'
+        );
+        require(escrowDetail.deadline >= block.timestamp, '___EXPIRED___');
+        escrowDetail.status = EscrowStatus.Ongoing;
+        escrowDetail.deadline = escrowDetail.requestRevisedDeadline;
+    }
+
+    function cancel() external {
+        require(uint8(escrowDetail.status) < 4, '___NOT_ELIGIBLE___');
+        require(
+            msg.sender == escrowDetail.buyer ||
+                msg.sender == escrowDetail.seller,
+            '___INVALID_BUYER_SELLER___'
+        );
+
+        if (
+            msg.sender == escrowDetail.buyer &&
+            (escrowDetail.status == EscrowStatus.Ongoing ||
+                escrowDetail.status == EscrowStatus.RequestRevised ||
+                escrowDetail.status == EscrowStatus.Delivered)
+        ) {
+            require(
+                escrowDetail.deadline <= block.timestamp,
+                '___NOT_EXPIRED___'
+            );
+        }
+
+        sendAndStatusUpdate(escrowDetail.buyer, EscrowStatus.Cancelled);
+    }
+
+    function fund(address payable toFund) external trusted {
+        require(
+            toFund == escrowDetail.buyer || toFund == escrowDetail.seller,
+            '___INVALID_BUYER_SELLER___'
+        );
+        require(
+            EscrowStatus.Cancelled != escrowDetail.status,
+            '___ALREADY_CANCELLED___'
+        );
+        require(
+            escrowDetail.status != EscrowStatus.Complete,
+            '___NOT_IN_COMPLETE_STATUS___'
+        );
+        sendAndStatusUpdate(toFund, EscrowStatus.Complete);
+    }
+
+    function getDetails() public view returns (EscrowDetail memory escrow) {
+        return escrowDetail;
+    }
+
+    modifier onlyBuyer() {
+        require(msg.sender == escrowDetail.buyer, '___ONLY_BUYER___');
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == escrowDetail.seller, '___ONLY_SELLER___');
+        _;
+    }
+
+    modifier trusted() {
+        require(areTrustedHandlers[msg.sender], '___NOT_TRUSTED___');
+        _;
+    }
+}
---
+++ EscrowFactory.sol
@@ -0,0 +1,198 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import './Escrow.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+contract EscrowFactory {
+    uint8 feePercent = 1;
+
+    uint256 public counter;
+    mapping(address => address[]) public myEscrows;
+
+    address[] escrows;
+
+    address[] processedTrustedHandlers;
+    mapping(address => bool) public areTrustedHandlers;
+    mapping(address => bool) public areTokenTrusted;
+
+    address feeAddress = address(this);
+
+      
+    event Created(address);
+
+    constructor(address _backup, address[] memory trustedTokens) {
+        processedTrustedHandlers.push(msg.sender);
+        areTrustedHandlers[msg.sender] = true;
+        processedTrustedHandlers.push(_backup);
+        areTrustedHandlers[_backup] = true;
+        areTokenTrusted[address(0)] = true; // for native currencies
+        switchActiveTrustedTokens(trustedTokens, true);
+    }
+
+    function createEscrow(
+        address payable seller,
+        address tokenAddress,
+        uint256 amount,
+        bytes32 title,
+        bytes32 shortInfo,
+        uint256 _standartDuration
+    ) external payable returns (address) {
+        require(msg.sender != seller, '___INVALID_SAME___');
+        require(seller != address(0), '___NON_EXIST_ADDRESS___');
+        require(areTokenTrusted[tokenAddress], '___NOT_TRUSTED___');
+
+        IERC20 token = IERC20(tokenAddress);
+        if (tokenAddress != address(0)) {
+            require(
+                token.balanceOf(msg.sender) >= amount,
+                '___TOKEN_UNAVAILABLE___'
+            );
+        } else {
+            require(msg.value == amount, '___DIFFER_AMOUNT_VAL___');
+        }
+
+        Escrow escrow = new Escrow(
+            payable(feeAddress),
+            tokenAddress,
+            _standartDuration,
+            amount,
+            title,
+            shortInfo,
+            payable(msg.sender),
+            seller,
+            feePercent,
+            getTrustedHandlers()
+        );
+
+        if (tokenAddress == address(0)) {
+            payable(address(escrow)).transfer(msg.value);
+        } else {
+            token.transferFrom(msg.sender, address(escrow), amount);
+        }
+
+        escrows.push(address(escrow));
+        myEscrows[msg.sender].push(address(escrow));
+        myEscrows[seller].push(address(escrow));
+        emit Created(address(escrow));
+        return address(escrow);
+    }
+
+    function getTrustedHandlers() public view returns (address[] memory) {
+    // function getTrustedHandlers() private view returns (address[] memory) {
+        address[] memory trustedHandlers = new address[](processedTrustedHandlers.length);
+        uint j = 0;
+        for (uint i = 0; i < processedTrustedHandlers.length; i++) {
+            if (areTrustedHandlers[processedTrustedHandlers[i]]) {
+                trustedHandlers[j] = processedTrustedHandlers[i];
+                j++;
+            }
+        }
+        return trustedHandlers;
+    }
+
+    function withdraw(address payable to, address tokenAddress, uint256 amount)
+        external
+        payable
+        trusted
+    {   
+        if (tokenAddress == address(0)) {
+            to.transfer(amount);
+        } else {
+            IERC20 token = IERC20(tokenAddress);
+            token.transfer(to, amount);
+        }
+    }
+
+    function switchActiveTrustedHandlers(
+        address[] memory _handlers,
+        bool approve
+    ) public trusted {
+        for (uint256 i = 0; i < _handlers.length; i++) {
+            processedTrustedHandlers.push(_handlers[i]);
+            areTrustedHandlers[_handlers[i]] = approve;
+        }
+    }
+
+    function switchActiveTrustedTokens(address[] memory _tokens, bool approve)
+        public
+        trusted
+    {
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            areTokenTrusted[_tokens[i]] = approve;
+        }
+    }
+
+    function checkTrusted(address _addr) public view trusted returns (bool) {
+        return areTrustedHandlers[_addr];
+    }
+
+    function checkTrustedToken(address _token)
+        public
+        view
+        trusted
+        returns (bool)
+    {
+        return areTokenTrusted[_token];
+    }
+
+    function getFee() public view trusted returns (uint8) {
+        return feePercent;
+    }
+
+    function updateFeePercent(uint8 _feePercent) external trusted {
+        require(
+            _feePercent > 0 || _feePercent < 100,
+            '___INVALID_FEE_PERCENT___'
+        );
+        feePercent = _feePercent;
+    }
+
+    function updateFeeAddress(address _feeAddress) external trusted {
+        feeAddress = _feeAddress;
+    }
+
+    fallback() external payable {}
+
+    receive() external payable {}
+
+    function balanceOf(address tokenAddress) public view returns (uint256) {
+        if (tokenAddress == address(0)) {
+            return address(this).balance;
+        }
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function getMyEscrows()
+        public
+        view
+        returns (address[] memory escrowAddresses)
+    {
+        return myEscrows[msg.sender];
+    }
+
+    // recent to oldest
+    function getEscrowDetailsPaging(uint256 offset)
+        external
+        view
+        trusted
+        returns (address[] memory escrowAddresses, uint256 total)
+    {
+        uint256 limit = 10;
+        if (limit > escrows.length - offset) {
+            limit = escrows.length - offset;
+        }
+
+        address[] memory values = new address[](limit);
+        for (uint256 i = 0; i < limit; i++) {
+            values[i] = escrows[escrows.length - 1 - offset - i];
+        }
+
+        return (values, escrows.length);
+    }
+
+    modifier trusted() {
+        require(areTrustedHandlers[msg.sender], '___NOT_TRUSTED___');
+        _;
+    }
+}
